4b5f77a88ee477108750c1cb37c63d758593118e
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-05-13 01:17:48 +0900

    "removed all unnecessary css lines"

--- src/component/page/Unity.tsx
+++ src/component/page/Unity.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import Header from '../Header';
+import Header from '../Header/Header';
 import SignUp from '../SignUp';
 
 export default function Unity(){
