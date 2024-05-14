3162a11128da7a205f542f488074f2a41eebc93f
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-05-14 13:05:45 +0900

    successfully get Student Id and PW in Login.tsx

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,46 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import Header from './Header/Header';
 import SignUp from './SignUp';
 
-const Login = () =>{
+const Login = () => {
+    const [acc, setAcc] = useState(''); // State for the account
+    const [pwd, setPwd] = useState(''); // State for the password
 
-    const click = () => {
-        alert('clicked');
+    function submit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault(); 
+        console.log("submitted " + acc + " " + pwd);
+    }
+
+    // Update account state
+    const accUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAcc(e.target.value);
     };
 
-	function submit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        var acc;
-        var pwd;
-	    console.log("submitted" + acc + pwd);
-	}
-	  
+    // Update password state
+    const pwdUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPwd(e.target.value);
+    };
+
     return (
-		<>
-		    <Header/>
-            {/* Login seciton */}
+        <>
+            <Header/>
+            {/* Login section */}
             <section className="container">
                 <form id="my-form" onSubmit={submit}>
                     <h1>Login</h1>
                     <div className="msg"></div>
                     <div>
-                    <label for="name">Student ID:</label>
-                    <input type="text" id="name"></input>
+                        <label htmlFor="name">Student ID:</label>
+                        <input type="text" id="name" value={acc} onChange={accUpdate}></input>
                     </div>
                     <div>
-                    <label for="email">Password:</label>
-                    <input type="text" id="email"></input>
+                        <label htmlFor="pwd">Password:</label>
+                        <input type="password" id="pwd" value={pwd} onChange={pwdUpdate}></input>
                     </div>
                     <input className="btn" type="submit" value="Submit"></input>
                 </form>
 
                 <ul id="users"></ul>
-
-                
             </section>
-
-		</>
+        </>
     );
 }
 
