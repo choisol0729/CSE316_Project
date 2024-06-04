51cb4bed64be337b1f8d65546955f927199f9c64
choisol0729 <choisol0729@gmail.com>
2024-06-04 21:29:41 +0900

    Minor package removal

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,20 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from "react-router-dom";
 import Header from './Header/Header';
 
 export default function Main(){
-    const click = () => {
-        alert('clicked');
-      };
-	function myFunction(e: React.ChangeEvent<HTMLInputElement>) {
-	console.log(e.target.value); // 현재 입력 필드의 값을 콘솔에 출력
-	}
 	  
     return (
 		<>
 
-		<Header/>
-		<h1>Main Page</h1>
+            <Header/>
+            <h1>Main Page</h1>
 
 		</>
     );
