bae54b786b3597d75e589cecf08b1dfe8b6bada2
choisol0729 <choisol0729@gmail.com>
2024-06-05 20:11:41 +0900

    photo css fixed to 100 px 100 px

--- src/component/clickedPages/ClickedPage.tsx
+++ src/component/clickedPages/ClickedPage.tsx
@@ -4,14 +4,11 @@ export default function Main(){
 	  
     return (
 		<>
-
-      <Header/>
-      <h1 style={{color:'white'}}>clicked Page</h1>
-      <section className="container">
-          <img id="hackathonImg" src="../../imgs/HACKATHON.png" alt="" />
-      </section>
-      
-
+            <Header/>
+            <h1 style={{color:'white'}}>clicked Page</h1>
+            <section className="container">
+                <img id="hackathonImg" src="../../imgs/HACKATHON.png" alt="" />
+            </section>
 		</>
     );
 }
