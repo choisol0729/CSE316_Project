2075191549e99eb43c732779606fb178ce728b38
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-06-05 10:19:51 +0900

    deletePage css changed

--- src/component/Delete.tsx
+++ src/component/Delete.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import Header from './Header/Header';
 import axios from 'axios';
-
+import "../App.css";
 export default function Delete(){
     
     const acc = sessionStorage.getItem('userId')
@@ -25,10 +25,14 @@ export default function Delete(){
 		<>
 
 		<Header/>
-		    <h1>Delete Account</h1>
-            <form onSubmit={submit}>
-                <input type="submit" value="Delete Account" />
-            </form>
+		    
+            <section className='container'>
+                
+                <form id="my-form" onSubmit={submit}>
+                    <h1>Delete Account</h1>
+                    <input className="btn" type="submit" value="Delete Account" />
+                </form>
+            </section>
 		</>
     );
 }
