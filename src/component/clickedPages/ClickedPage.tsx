67cd24a9749470af174db0683c5b6f4fa6fcbe02
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-06-05 20:56:15 +0900

    using location to get data

--- src/component/clickedPages/ClickedPage.tsx
+++ src/component/clickedPages/ClickedPage.tsx
@@ -1,14 +1,25 @@
 import Header from '../Header/Header';
 // import '../App.css'
+import { useLocation, useNavigate } from 'react-router-dom';
 export default function ClickedPage(){
 	  
+  const location = useLocation();
+  const userInfo = { ...location.state };
+  const navigate = useNavigate();
+
     return (
 		<>
             <Header/>
             <h1 style={{color:'white'}}>clicked Page</h1>
-            <section className="container">
-                <img id="hackathonImg" src="../../imgs/HACKATHON.png" alt="" />
-            </section>
+            <div>
+              {userInfo.id}
+            </div>
+            <div>
+              {userInfo.content}
+            </div>
+            <div>
+              {userInfo.title}
+            </div>
 		</>
     );
 }
