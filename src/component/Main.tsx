ad6452f7542ffc6ff3784ae15d0adea854314a02
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-06-05 17:09:40 +0900

    decorated main page

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,12 +1,16 @@
 import Header from './Header/Header';
-
+import '../App.css'
 export default function Main(){
 	  
     return (
 		<>
 
-            <Header/>
-            <h1>Main Page</h1>
+      <Header/>
+      <h1>Main Page</h1>
+      <section className="container">
+          <img src="../../imgs/HACKATHON.png" alt="" />
+      </section>
+      
 
 		</>
     );
