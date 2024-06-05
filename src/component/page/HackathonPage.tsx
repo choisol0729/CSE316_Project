bcd0db8bab9ec325fd87489d23b2785e3b790abe
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-06-05 22:18:59 +0900

    hackathon can clikc wow

--- src/component/page/HackathonPage.tsx
+++ src/component/page/HackathonPage.tsx
@@ -1,49 +1,102 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, MouseEventHandler } from 'react';
+import { useNavigate,useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
+import './page.css'
+import axios from 'axios'
 
 interface BlogPost {
     id: number;
     title: string;
     content: string;
     category: string;
+    url: string;
 }
 
-const Hackathon = () => {
+const HackathonPage = () => {
     const [posts, setPosts] = useState<BlogPost[]>([]);
     const navigate = useNavigate();
+    const [fetchedPosts, setFetchedPosts] = useState<{ title: string; category: string; content:string; id:number;}[]>([]);
 
     useEffect(() => {
-        const allPostsString = sessionStorage.getItem('allPosts');
-        if (allPostsString) {
-            const allPosts = JSON.parse(allPostsString) as BlogPost[];
-            const hackathonPosts = allPosts.filter(post => post.category === 'Hackathon');
-            setPosts(hackathonPosts);
-        }
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get<BlogPost[]>('http://localhost:2424/getAllContents');
+                setFetchedPosts(response.data);
+                const hackathonPosts = response.data.filter(post => post.category === 'Hackathon');
+                setPosts(hackathonPosts)
+            } catch (error) {
+                console.error('Error fetching posts', error);
+            }
+        };
+
+        fetchPosts();
     }, []);
+    
 
     const moveToEditPage = () => {
-        navigate('/edit');
+        const storedUserId = sessionStorage.getItem('userId'); 
+        console.log(storedUserId);
+        if(storedUserId!= null){
+            navigate('/edit');
+        }else{
+            alert('Login First')
+        }
+        
     }
+
+    const toClickedPage = async (id: number) => {
+        const response = await axios.get<BlogPost[]>('http://localhost:2424/getAllContents');
+        setFetchedPosts(response.data);
+
+        console.log("ID: ", id);
+
+        const hackathonPosts = response.data.filter(post => post.category === 'AI');
+        setPosts(hackathonPosts)
+        navigate('/clickedPage', { state: {
+            id: id
+        }})
+    }
+
+    
 
     return (
         <>
             <Header />
-            <div style={{ padding: '20px' }}>
-                <h1 style={{ color: 'white' }}>Hackathon Page</h1>
+            {/* <div className='form-group'>
+                wow
+            </div> */}
+
+            <div className="center" style={{ padding: '20px' }}>
+                <h1 style={{ color: 'white' }}>App Page</h1>
                 <button style={{ color: 'white'}} onClick={moveToEditPage}>Add New Post</button>
                 <div style={{ marginTop: '20px' }}>
                     {posts.length > 0 ? (
                         posts.map(post => (
                             <div key={post.id} style={{
-                                backgroundColor: 'white',
-                                color: 'black',
-                                marginBottom: '10px',
-                                padding: '10px',
-                                borderRadius: '5px'
+                                backgroundColor: 'white', 
+                                color: 'black', 
+                                marginBottom: '10px', 
+                                padding: '10px', 
+                                borderRadius: '5px',
+
+                                // display: 'flex',
+                                justifyContent: 'space-between',
                             }}>
-                                <h2>{post.title}</h2>
-                                <p>{post.content}</p>
+                                {/* check possible to click page or not */}
+                                
+                                <div onClick={() => toClickedPage(post.id)}>
+                                    <section>
+                                        <h2>{post.title}</h2>
+                                        {/* <p>{post.category}</p> */}
+                                        <p>{post.content}</p>
+                                    </section>
+                                    <br />
+                                    <section>
+                                        {post.url !== "" ? (<img src={post.url} className='sinom' alt=""/>) : <></>}
+                                    </section>
+                                </div>
+                                
+                                
                             </div>
                         ))
                     ) : (
@@ -55,4 +108,4 @@ const Hackathon = () => {
     );
 }
 
-export default Hackathon;
+export default HackathonPage;
