8b0a1ea588c9c9729f96659556f827b7e52fee9c
jaehyeok <hyeonjaehyeog9@gmail.com>
2024-06-05 20:47:11 +0900

    appPage

--- src/component/page/AppPage.tsx
+++ src/component/page/AppPage.tsx
@@ -1,49 +1,102 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
+import './page.css'
+import axios from 'axios'
 
 interface BlogPost {
     id: number;
     title: string;
     content: string;
     category: string;
+    url: string;
 }
 
-const AppPage  = () => {
+const AppPage = () => {
     const [posts, setPosts] = useState<BlogPost[]>([]);
     const navigate = useNavigate();
+    const [fetchedPosts, setFetchedPosts] = useState<{ title: string; category: string; content:string; id:number;}[]>([]);
 
     useEffect(() => {
-        const allPostsString = sessionStorage.getItem('allPosts');
-        if (allPostsString) {
-            const allPosts = JSON.parse(allPostsString) as BlogPost[];
-            const appPosts = allPosts.filter(post => post.category === 'App');
-            setPosts(appPosts);
-        }
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get<BlogPost[]>('http://localhost:2424/getAllContents');
+                setFetchedPosts(response.data);
+                const aiPosts = response.data.filter(post => post.category === 'App');
+                setPosts(aiPosts)
+            } catch (error) {
+                console.error('Error fetching posts', error);
+            }
+        };
+
+        fetchPosts();
     }, []);
+    
+    // useEffect(() => {
+    //     const allPostsString = sessionStorage.getItem('allPosts');
+    //     if (allPostsString) {
+    //         const allPosts = JSON.parse(allPostsString) as BlogPost[];
+    //         const aiPosts = allPosts.filter(post => post.category === 'AI');
+    //         setPosts(aiPosts);
+    //     }
+    // }, []);
 
     const moveToEditPage = () => {
         navigate('/edit');
     }
 
+    const toClickedPage = () => {
+        const fetchPosts = async () => {
+            const response = await axios.get<BlogPost[]>('http://localhost:2424/getAllContents');
+            setFetchedPosts(response.data);
+            const aiPosts = response.data.filter(post => post.category === 'App');
+            setPosts(aiPosts)
+            navigate('/clickedPage', { state: {
+                
+            }})
+        }
+    }
+
+    
+
     return (
         <>
             <Header />
-            <div style={{ padding: '20px' }}>
+            {/* <div className='form-group'>
+                wow
+            </div> */}
+
+            <div className="center" style={{ padding: '20px' }}>
                 <h1 style={{ color: 'white' }}>App Page</h1>
                 <button style={{ color: 'white'}} onClick={moveToEditPage}>Add New Post</button>
                 <div style={{ marginTop: '20px' }}>
                     {posts.length > 0 ? (
                         posts.map(post => (
                             <div key={post.id} style={{
-                                backgroundColor: 'white',
-                                color: 'black',
-                                marginBottom: '10px',
-                                padding: '10px',
-                                borderRadius: '5px'
+                                backgroundColor: 'white', 
+                                color: 'black', 
+                                marginBottom: '10px', 
+                                padding: '10px', 
+                                borderRadius: '5px',
+
+                                // display: 'flex',
+                                justifyContent: 'space-between',
                             }}>
-                                <h2>{post.title}</h2>
-                                <p>{post.content}</p>
+                                {/* check possible to click page or not */}
+                                
+                                <div onClick={toClickedPage}>
+                                    <section>
+                                        <h2>{post.title}</h2>
+                                        {/* <p>{post.category}</p> */}
+                                        <p>{post.content}</p>
+                                    </section>
+                                    <br />
+                                    <section>
+                                        {post.url !== "" ? (<img src={post.url} className='sinom' alt=""/>) : <></>}
+                                    </section>
+                                </div>
+                                
+                                
                             </div>
                         ))
                     ) : (
